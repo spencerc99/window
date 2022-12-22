4cf857ef83a3ab839c3982e4cb76005422bf7849
Spencer Chang <spencerc99@gmail.com>
2022-12-22 15:45:56 -0600

    make submit page actually show up

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       // https://rollupjs.org/guide/en/#big-list-of-options
+      input: {
+        main: resolve(__dirname, "index.html"),
+        submit: resolve(__dirname, "submit.html"),
+      },
     },
   },
   base: "./",
